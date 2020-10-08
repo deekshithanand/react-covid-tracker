3da71687723800da42e441b6bcd04f68940facdb
deekshithanand <deekshith27anand@gmail.com>
2020-10-08 22:07:36 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Cards, Chart, Country } from './components'
+import Styles from './App.module.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+
+    render() {
+        return (
+            <div className={Styles.container}>
+                <h1>
+                    APP WOrking~
+            </h1>
+                <Cards />
+                <Chart />
+                <Country />
+            </div>
+
+        )
+    }
 }
-
-export default App;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import  Cards from './Cards/Cards'
+import  Chart from './Chart/Chart'
+import Country from './Country/Country'
+
+export {Cards,Chart,Country}
